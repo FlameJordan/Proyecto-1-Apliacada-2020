877bafda60de9a9dc5bd0eaad36d9f765bfdb311
Eyleen <eyleenqh@hotmail.com>
2020-09-14 22:16:58 -0600

    Parte proveedores completa

---
+++ Proveedores/TiendaBazar/TiendaBazar/Models/KeyRequest.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TiendaBazar.Models
+{
+    public class KeyRequest
+    {
+        public string claveGeneradaField { get; set; }
+        public string PropertyChanged { get; set; }
+    }
+}
